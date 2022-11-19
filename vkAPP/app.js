7419f26bf88fa39c78f176e38768b82933d868ab
Walek-dot <plymouth_baracuda@mail.ru>
2022-11-19 15:31:40 -0700

    second commit

--- vkAPP/app.js
+++ vkAPP/app.js
@@ -1,1 +1,8 @@
-alert('BAZIN loh')
+$(document).ready(function(){
+    let tableId = document.querySelector('Table__Grid');
+    let td = document.createElement('td');
+    td.className = "Table__Cell Table__Cell--fixed Table__Cell--header Table__Cell--hasSort Table__Cell--fixedColumn Table__Cell--name_view_column Table__Cell--alignleft";
+    td.innerHTML = "Прослушивания";
+    tableId.appendChild(td);
+    alert("BAzin loh");
+    });
