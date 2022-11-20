d322c0161055eaabd4f00b9f4c779683a972b97a
Walek-dot <plymouth_baracuda@mail.ru>
2022-11-19 18:44:47 -0700

    three commit

--- vkAPP/app.js
+++ vkAPP/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    let tableId = document.querySelector('Table__Grid');
+    let tableId = document.getElementById('Table__wrapper');
     let td = document.createElement('td');
     td.className = "Table__Cell Table__Cell--fixed Table__Cell--header Table__Cell--hasSort Table__Cell--fixedColumn Table__Cell--name_view_column Table__Cell--alignleft";
     td.innerHTML = "Прослушивания";
